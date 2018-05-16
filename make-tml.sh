9c83c3d7dfdd2dacf465d11b0d05396056b480f3
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-05-16 05:54:56 +0200

    added echo at the end

--- make-tml.sh
+++ make-tml.sh
@@ -5,3 +5,4 @@ file=timelapse-$(date -u +\%Y\%m\%d)-30fps.mp4;
 
 cd /home/pi/timelapse/img;
 time ffmpeg -threads 0 -framerate 30 -r 30 -s 1600x1600 -pattern_type glob -i './*.png' -vcodec libx264 -tune grain -preset fast -pix_fmt yuv420p -b:v 20M -y /home/pi/timelapse/$file;
+echo "timelapse made for" $(date -u +\%Y\%m\%d);
