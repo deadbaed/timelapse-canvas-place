8feccd2e60644600a10fb4c5d06059fc4ed526d0
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-05-16 05:47:50 +0200

    added where to go

--- archive-xz.sh
+++ archive-xz.sh
@@ -3,6 +3,7 @@
 
 #tar -cvJf img-$(date -u +\%Y\%m\%d).tar.xz img/;
 
+cd /home/pi/timelapse;
 #use as many cores available with "-T 0" (see xz --help)
 time tar -cvf - img/ | xz -T 0 > img-$(date -u +\%Y\%m\%d).tar.xz;
 echo "archived in tar.xz";
