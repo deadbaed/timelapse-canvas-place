82d12a1bf074af1d3040697b3f4545bde2ad77cd
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-05-16 05:51:49 +0200

    final folder + date in the filename

--- make-tml.sh
+++ make-tml.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-file=timelapse-30fps-$(date -u +\%Y\%m\%d).mp4;
+file=timelapse-$(date -u +\%Y\%m\%d)-30fps.mp4;
 
 cd /home/pi/test-place/96files;
-time ffmpeg -threads 0 -framerate 30 -r 30 -s 1600x1600 -pattern_type glob -i './*.png' -vcodec libx264 -tune grain -preset fast -pix_fmt yuv420p -b:v 20M -y ../$file;
+time ffmpeg -threads 0 -framerate 30 -r 30 -s 1600x1600 -pattern_type glob -i './*.png' -vcodec libx264 -tune grain -preset fast -pix_fmt yuv420p -b:v 20M -y /home/pi/timelapse/$file;
