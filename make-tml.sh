09d5e3c0873ebb25a1bf98606e952dfe13dd5e71
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-05-16 05:21:22 +0200

    added date in filename

--- make-tml.sh
+++ make-tml.sh
@@ -1,1 +1,6 @@
-time ffmpeg -threads 0 -framerate 30 -r 30 -s 1600x1600 -pattern_type glob -i './*.png' -vcodec libx264 -tune grain -preset fast -pix_fmt yuv420p -b:v 20M -y '../timelapse-30fps.mp4'
+#!/bin/bash
+#
+
+file=../timelapse-30fps-$(date -u +\%Y\%m\%d).mp4;
+
+time ffmpeg -threads 0 -framerate 30 -r 30 -s 1600x1600 -pattern_type glob -i './*.png' -vcodec libx264 -tune grain -preset fast -pix_fmt yuv420p -b:v 20M -y $file;
