4a8536187319cf4b08b7c78cf2dea392c01d4abc
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-05-16 05:53:08 +0200

    real folder

--- make-tml.sh
+++ make-tml.sh
@@ -3,5 +3,5 @@
 
 file=timelapse-$(date -u +\%Y\%m\%d)-30fps.mp4;
 
-cd /home/pi/test-place/96files;
+cd /home/pi/timelapse/img;
 time ffmpeg -threads 0 -framerate 30 -r 30 -s 1600x1600 -pattern_type glob -i './*.png' -vcodec libx264 -tune grain -preset fast -pix_fmt yuv420p -b:v 20M -y /home/pi/timelapse/$file;
