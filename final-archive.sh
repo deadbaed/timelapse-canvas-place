0ec04eb92d769ee66f2e5728563c389e43c0b5c9
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-05-17 05:57:30 +0200

    final archive works!

--- final-archive.sh
+++ final-archive.sh
@@ -3,5 +3,12 @@
 
 folder=/home/pi/timelapse/arc/timelapse-$(date -u +\%Y\%m\%d);
 
+cd /home/pi/timelapse;
+
 mkdir -p $folder;
 echo "folder" $folder "created";
+
+mv timelapse-$(date -u +\%Y\%m\%d)-30fps.mp4 $folder;
+mv img-$(date -u +\%Y\%m\%d).tar.xz $folder;
+
+echo "moved timelapse and images for" $(date -u +\%Y\%m\%d) "in archive folder";
