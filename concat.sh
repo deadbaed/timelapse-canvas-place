3c72e7b79231c1ec220b0e6070df0908b7f6b4e5
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-05-17 05:48:42 +0200

    added echo

--- concat.sh
+++ concat.sh
@@ -5,11 +5,14 @@ cd /home/pi/timelapse;
 
 # copy current timelapse as old to not overwrite it when running ffmpeg
 cp timelapse.mp4 timelapse-old.mp4;
+echo "copied timelapse.mp4 to timelapse-old.mp4"
 
 time ffmpeg -f concat -i concat-files.txt -c copy -y /home/pi/timelapse/timelapse.mp4;
 
 # rename 'timelapse-new.mp4' to put the current date for the archives
 mv timelapse-new.mp4 timelapse-$(date -u +\%Y\%m\%d)-30fps.mp4;
+echo "moved timelapse-new.mp4 to have the date"
 
 # remove 'timelapse-old.mp4' cause no longer neeeded (plus there is backups for every day)
 rm timelapse-old.mp4
+echo "removed timelapse-old.mp4"
