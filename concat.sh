1bc7cbc6dee796d9448c11df808ef15b37d95dc1
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-05-17 05:28:37 +0200

    added concat-files.txt

--- concat.sh
+++ concat.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 #
 
+cd /home/pi/timelapse;
+
+# copy current timelapse as old to not overwrite it when running ffmpeg
+cp timelapse.mp4 timelapse-old.mp4 
+
+time ffmpeg -f concat -i concat-files.txt -c copy -y /home/pi/timelapse/timelapse.mp4;
