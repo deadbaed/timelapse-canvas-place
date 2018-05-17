a25d741c8603f40ad41778366c66f1b9b7b0088c
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-05-17 05:33:57 +0200

    fixes

--- make-tml.sh
+++ make-tml.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 #
 
-file=timelapse-$(date -u +\%Y\%m\%d)-30fps.mp4;
-
 cd /home/pi/timelapse;
-time ffmpeg -threads 0 -framerate 30 -r 30 -s 1600x1600 -pattern_type glob -i 'img/*.png' -vcodec libx264 -tune grain -preset fast -pix_fmt yuv420p -b:v 20M -y /home/pi/timelapse/timelapse-new.mp4;
-echo "timelapse made";
+time ffmpeg -threads 0 -framerate 30 -r 30 -s 1600x1600 -pattern_type glob -i 'img/*.png' -vcodec libx264 -tune grain -preset fast -pix_fmt yuv420p -b:v 20M -y timelapse-new.mp4;
+echo "timelapse-new made";
