46a06fc7caf6f5a735db9ba9fd31f5d583ee9341
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-05-17 06:00:55 +0200

    adding a do it all script

---
+++ do-it-all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+
+echo "1. archive images"
+echo "2. make the timelapse of the day"
+echo "3. delete old images"
+echo "4. concat the old and new timelapses"
+echo "5. archive the video and the images"
