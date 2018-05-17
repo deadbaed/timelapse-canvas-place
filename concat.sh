f3106f6787a34b484abfa947df2912185ae73d54
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-05-17 05:34:03 +0200

    added copying and renaming files before and after concating

--- concat.sh
+++ concat.sh
@@ -4,6 +4,9 @@
 cd /home/pi/timelapse;
 
 # copy current timelapse as old to not overwrite it when running ffmpeg
-cp timelapse.mp4 timelapse-old.mp4 
+cp timelapse.mp4 timelapse-old.mp4;
 
 time ffmpeg -f concat -i concat-files.txt -c copy -y /home/pi/timelapse/timelapse.mp4;
+
+# rename 'timelapse-new.mp4' to put the current date for the archives
+mv timelapse-new.mp4 timelapse-$(date -u +\%Y\%m\%d)-30fps.mp4;
