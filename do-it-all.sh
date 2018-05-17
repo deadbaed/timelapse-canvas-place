3fcc2d35b28c009ed4c049d01bd8bc59e3c8bf91
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-05-17 06:36:01 +0200

    hopefully this works

--- do-it-all.sh
+++ do-it-all.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 #
 
+cd /home/pi/wkf;
+
 echo "1. archive images"
+./archive-xz.sh
+
 echo "2. make the timelapse of the day"
+./make-tml.sh
+
 echo "3. delete old images"
+./delete-img.sh
+
 echo "4. concat the old and new timelapses"
+./concat.sh
+
 echo "5. archive the video and the images"
+./final-archive.sh
+
+echo "done, seeya tomorrow :)"
+echo "made with love by http://philippeloctaux.com"
