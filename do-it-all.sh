662a967db97d7dba083a626c73df5a87a7a7946b
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-07-09 03:59:58 +0200

    fuck ffmpeg on 1gb ram

--- do-it-all.sh
+++ do-it-all.sh
@@ -7,19 +7,19 @@ echo "1. archive images"
 ./archive-xz.sh
 
 echo "2. make the timelapse of the day"
-./make-tml.sh
+#./make-tml.sh
 
 echo "3. delete old images"
 ./delete-img.sh
 
 echo "4. concat the old and new timelapses"
-./concat.sh
+#./concat.sh
 
 echo "5. archive the video and the images"
 ./final-archive.sh
 
 echo "6. upload the timelapse to the vps and rename the final timelapse"
-./vps-and-publish.sh
+#./vps-and-publish.sh
 
 echo "done, seeya tomorrow :)"
 echo "made with love by http://philippeloctaux.com"
