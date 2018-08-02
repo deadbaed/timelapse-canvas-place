5d61058974ddb949ce54ecf1bbfd189d22c563f7
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-08-02 12:28:45 +0200

    removing backups on remote

--- pi/3-delete-remote-backups.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#
-
-ssh vps "rm -rfv /home/x4m3/web/13370403/timelapse/arc/timelapse-$(date -u +\%Y\%m\%d)"
--- pi/pi-do-it-all.sh
+++ pi/pi-do-it-all.sh
@@ -5,4 +5,3 @@ cd /home/pi/wkf;
 
 ./pi/1-download-backups.sh;
 ./pi/2-download-timelapse.sh;
-./pi/3-delete-remote-backups.sh;
