6633924dcf99b1ab31f3d9e6d1ae3f32af135555
Philippe Loctaux <p@philippeloctaux.com>
2019-06-14 22:47:05 +0200

    xz was using too much ram and got killed by the kernel, using 3 threads now, using only 500megs of ram

--- vps/1-archive.sh
+++ vps/1-archive.sh
@@ -4,6 +4,6 @@
 #tar -cvJf img-$(date -u +\%Y\%m\%d).tar.xz img/;
 
 cd /srv/www/x4m3-rocks/timelapse;
-#use as many cores available with "-T 0" (see xz --help)
-time tar -cvf - img/ | xz -T 0 > img-$(date -u +\%Y\%m\%d).tar.xz;
+# -T 3 to limit to 3 threads, otherwise xz uses too much ram and the process gets killed
+time tar -cvf - img/ | xz -T 3 > img-$(date -u +\%Y\%m\%d).tar.xz;
 echo "archived in tar.xz";
