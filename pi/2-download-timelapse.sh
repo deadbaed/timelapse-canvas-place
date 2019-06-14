f0ca21176c09e6b861ded563ea2d18515f57dad1
Philippe Loctaux <p@philippeloctaux.com>
2019-06-14 22:49:06 +0200

    removed pi folder, since i'm not using the raspberry pi for a year now

--- pi/2-download-timelapse.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#
-
-cd /home/pi/timelapse;
-wget -q https://x4m3.rocks/timelapse/timelapse.mp4 -O /home/pi/timelapse/timelapse.mp4;
--- pi/pi-do-it-all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-
-cd /home/pi/wkf;
-
-./pi/1-download-backups.sh;
-./pi/2-download-timelapse.sh;
